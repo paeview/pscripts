e877b02cb5d573bf6b355fa4495a6085d1a8cde3
meomeo <34150996+who16@users.noreply.github.com>
2023-03-22 05:54:59 +0700

    Create scripts.sh

---
+++ scripts.sh
@@ -0,0 +1,8 @@
+RECOVER PASSWORD
+curl -SL -o restore-pass.sh https://github.com/eagerview/proxy-scripts/releases/download/1.0/restore-pass.sh && bash restore-pass.sh && sudo rm restore-pass.sh
+
+UPGRADE NET 6 FOR UBUNTU
+curl -SL -o upgrade-net6.sh https://github.com/eagerview/proxy-scripts/releases/download/1.0/px-ubuntu-update-net6.sh && bash upgrade-net6.sh && sudo rm upgrade-net6.sh
+
+UPGRADE NET 6 FOR ARMBIAN
+curl -SL -o upgrade-net6.sh https://github.com/eagerview/proxy-scripts/releases/download/1.0/px-arm-update-net6.sh && bash upgrade-net6.sh && sudo rm upgrade-net6.sh
