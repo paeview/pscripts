11be78977f67c7a0bdff65947f4dd6da9a9fb035
meomeo <34150996+who16@users.noreply.github.com>
2023-03-24 11:20:42 +0700

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -6,3 +6,10 @@ curl -SL -o upgrade-net6.sh https://github.com/eagerview/proxy-scripts/releases/
 
 UPGRADE NET 6 FOR ARMBIAN
 curl -SL -o upgrade-net6.sh https://github.com/eagerview/proxy-scripts/releases/download/1.0/px-arm-update-net6.sh && bash upgrade-net6.sh && sudo rm upgrade-net6.sh
+
+
+** NOTE ARMBIAN **
+If dns not link, then use command
+sudo rm /etc/resolv.conf
+sudo ln -s /run/resolvconf/resolv.conf /etc/resolv.conf
+sudo resolvconf -u
